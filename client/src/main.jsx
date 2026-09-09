663d240bb08f8ba47d95f9d8ff1656ee10958800
anas <anasrehman390@gmail.com>
2026-09-09 19:39:10 +0500

    feat(root/main): add routes for root and fallback
    - Add route for root element as signup component
    - Add fallback route if any invalid url is entered

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom'
 import AuthLayout from './Layouts/auth.jsx'
 import SignUp from './Pages/Auth/SignUp.jsx'
 import Login from './Pages/Auth/Login.jsx'
@@ -11,7 +10,11 @@ import Login from './Pages/Auth/Login.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<AuthLayout />}>
+      {/* MAKING ROOT ELEMENT SIGNUP */}
+      <Route index element={<Navigate to={"/signup"} replace />} />
       <Route path='signup' element={<SignUp />} />
+      {/* FALLBACK FOR INVALID URLS */}
+      <Route path='*' element={<div>Page not found</div>} />
       <Route path='login' element={<Login />} />
     </Route>
   )
