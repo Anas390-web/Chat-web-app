f0f9a4995fb7e785d8f8e633855d83c0b4912019
anas <anasrehman390@gmail.com>
2026-09-09 19:18:21 +0500

    fix(Layout/auth): change import source
    -Change import source from react-router to react-router-dom

--- client/src/Layouts/auth.jsx
+++ client/src/Layouts/auth.jsx
@@ -1,10 +1,10 @@
-import { Outlet } from 'react-router'
+import { Outlet } from 'react-router-dom'
 
 function AuthLayout() {
    return (
-      <>
+      <div className='min-h-dvh overflow-hidden flex-col-center'>
          <Outlet />
-      </>
+      </div>
    )
 }
 
