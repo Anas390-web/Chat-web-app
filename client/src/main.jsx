099fd38f560a58af879aeafd84a93df337851cb7
anas <anasrehman390@gmail.com>
2026-09-09 16:30:09 +0500

    feat(root/main): add AuthLayout and setup routing
    - Configure createBrowserRouter with nested routes for /login and /signup
    - Wrap the application with RouterProvider in root index
    
    Create the router for AuthLayout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import AuthLayout from './Layouts/auth.jsx'
+import SignUp from './Pages/Auth/SignUp.jsx'
+import Login from './Pages/Auth/Login.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<AuthLayout />}>
+      <Route path='signup' element={<SignUp />} />
+      <Route path='login' element={<Login />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
