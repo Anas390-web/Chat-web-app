1c78bcfc34480c445ea0928a5181d141b22e7841
anas <anasrehman390@gmail.com>
2026-09-09 19:41:54 +0500

    feat(Pages/auth): Add structure and inline styles
    Create a whole structure of Sign Up component and add inline styles
    using Tailwind utitlity classes

--- client/src/Pages/Auth/SignUp.jsx
+++ client/src/Pages/Auth/SignUp.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { NavLink } from "react-router";
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="h-auto w-full sm:w-120 p-8 flex-col-start justify-between gap-2 shadow-2xl  bg-slate-50">
+      <h1>SignUp</h1>
+      <form className="flex flex-col gap-4 w-full">
+        <label className="flex flex-col gap-1.5 font-medium">
+          Username:
+          <input
+            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
+            type="text"
+            placeholder="Enter testing username" />
+        </label>
+        <label className="flex flex-col gap-1.5 font-medium">
+          Email:
+          <input
+            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
+            type="text"
+            placeholder="Enter testing email" />
+        </label>
+        <label className="flex flex-col gap-1.5 font-medium">
+          Password:
+          <input
+            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
+            type="password"
+            placeholder="Enter testing password" />
+        </label>
+        <label className="flex flex-col gap-1.5 font-medium">
+            Confirm Password:
+          <input
+            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
+            type="password"
+            placeholder="Enter testing password again" />
+        </label>
+        <button className="bg-black text-white p-1.5 rounded-sm cursor-pointer mt-2">SUBMIT</button>
+      </form>
+      <div className="flex gap-0.5">
+        <p>Already registered?</p>
+        <NavLink
+        to={'/login'}
+        >
+          Sign-in
+        </NavLink>
+      </div>
+    </div>
   )
 }
 
-export default SignUp
+export default SignUp;
