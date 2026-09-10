5e7dca68b6b6f1f7826448a0b77f2c54d62e2f21
anas <anasrehman390@gmail.com>
2026-09-10 16:53:19 +0500

    feat(Pages/Sidebar): create a sidebar component
    - Add structure and styling with Tailwind css to the Sidebar component.
    - Add custom made utility and component classes in tailwind.
    - Add Images and icons from the public folder.

---
+++ client/src/Pages/Dashboard/Static-Sidebar/Sidebar.jsx
@@ -0,0 +1,46 @@
+import Dashboard from '../../../public/images/Dashboard.png';
+import Chats from '../../../public/images/chats.png';
+import Groups from '../../../public/images/groups.png';
+import Settings from '../../../public/images/settings.png';
+import Archives from '../../../public/images/Archives.png';
+import { LightIcon, DarkIcon } from '../../../public/icons/Icons.jsx'
+
+
+function Sidebar() {
+  return (
+    <aside className='h-screen w-24 shrink-0 bg-gray-200 flex flex-col justify-between items-center px-2 py-1 font-semibold'>
+      <div className='flex flex-col gap-4'>
+        <div className='sidebar-icons'>
+          <img className='size-7' src={Dashboard} alt="Dashboard Icon" />
+        </div>
+        <hr />
+        <div className='sidebar-icons'>
+          <img className='size-7' src={Chats} alt="Chats Icon" />
+          <p>Chats</p>
+        </div>
+        <div className='sidebar-icons'>
+          <img className='size-7' src={Groups} alt="Groups Icon" />
+          <p>Groups</p>
+        </div>
+        <div className='sidebar-icons'>
+          <img className='size-7' src={Archives} alt="Archives Icon" />
+          <p>Archives</p>
+        </div>
+        <div className='sidebar-icons'>
+          <img className='size-7' src={Settings} alt="Settings Icon" />
+          <p>Settings</p>
+        </div>
+      </div>
+      <div className='flex flex-col justify-center items-center gap-4'>
+        <div className='sidebar-icons'>
+          <p>{<LightIcon />}</p>
+        </div>
+        <div className='sidebar-icons'>
+          <p>{<DarkIcon />}</p>
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
