992f8591d8bcfd29cd52cb07faf87962cfe86d68
anas <anasrehman390@gmail.com>
2026-09-10 16:41:40 +0500

    feat(Layout/dashboard): Make a dashboard layout
    - Create a Dashboard Layout.
    - Add Sidebar component as static
    - Add Dashboard component as dynamic in the Outlet component.

---
+++ client/src/Layouts/dashboard.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Pages/Dashboard/Static-Sidebar/Sidebar.jsx'
+
+function DashboardLayout() {
+   return (
+      <div className='min-h-dvh overflow-hidden flex'>
+         <Sidebar />
+         <Outlet />
+      </div>
+   )
+}
+
+export default DashboardLayout
