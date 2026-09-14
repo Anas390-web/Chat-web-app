3483b4dd43ae94f61b616c9c5e0676d7ebf16feb
anas <anasrehman390@gmail.com>
2026-09-14 18:05:31 +0500

    styles(Pages/Auth/Login): change styles
    - Change text color of Login title from text-blue-500 to text-amber-600
    - Change background color of Login button from bg-blue-500 to bg-amber-900

--- client/src/Pages/Auth/Login.jsx
+++ client/src/Pages/Auth/Login.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router"
 function Login() {
   return (
     <div className="h-auto w-full sm:w-120 p-8 flex-col-start justify-between gap-2 shadow-2xl  bg-slate-50">
-      <h1 className="text-blue-500">Login</h1>
+      <h1 className="text-amber-600">Login</h1>
       <form className="flex flex-col gap-4 w-full">
         <label className="flex flex-col gap-1.5 font-medium">
           Email:
@@ -19,7 +19,7 @@ function Login() {
             type="password"
             placeholder="Enter testing password" />
         </label>
-        <button className="bg-blue-500 text-white p-1.5 rounded-sm cursor-pointer mt-2">LOGIN</button>
+        <button className="text-white bg-amber-900 p-1.5 rounded-sm cursor-pointer mt-2">LOGIN</button>
       </form>
       <div className="flex gap-0.5">
         <p>No account?</p>
