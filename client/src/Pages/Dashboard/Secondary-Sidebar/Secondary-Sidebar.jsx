d736041a27feae6da4ece2fe62edfdac46b751ce
anas <anasrehman390@gmail.com>
2026-09-14 19:15:34 +0500

    styles(Pages/Dashboard): add styles to a comp
    - Add styles to the SecondarySidebar component inside the dashboard.
    - Add the KebabMenuIcon besides the title.
    - Add the settings image for a demo.

--- client/src/Pages/Dashboard/Secondary-Sidebar/Secondary-Sidebar.jsx
+++ client/src/Pages/Dashboard/Secondary-Sidebar/Secondary-Sidebar.jsx
@@ -1,23 +1,27 @@
-import settings from '../../../public/images/settings.png';
+import {KebabMenuIcon} from '../../../../public/icons/Icons';
+import settings from '../../../../public/images/settings.png';
 
 
 function SecondarySideBar() {
   return (
-    <aside className="h-screen w-screen sm:w-80 lg:w-100 flex flex-col justify-start px-2 py-1 font-semibold border">
+    <aside className="bg-gray-100 h-screen w-full sm:w-70 lg:w-100 flex flex-col justify-start font-semibold border">
       <div className="flex flex-col gap-2">
-        <div className=" h-16 flex items-center">
-          <h1>Bingo</h1>
+        <div className="bg-purple-300 h-20 flex items-center justify-between px-4 shrink-0">
+          <h1 className='tracking-wide uppercase'>chatty</h1>
+          <div>
+            <KebabMenuIcon />
+          </div>
         </div>
-        <div className=" h-10 flex items-center mb-1">
+        <div className="h-10 flex items-center mb-1 px-2">
           <h2>Messages (25)</h2>
         </div>
-        <div className=" h-10 w-full flex mb-1">
+        <div className=" h-10 w-full flex mb-1 px-2">
           <input
-            className="h-10 bg-gray-200 w-full p-2 rounded-lg"
+            className="h-10 bg-white w-full p-2 rounded-lg"
             type="text"
             placeholder="Search" />
         </div>
-        <div className='flex gap-2'>
+        <div className='flex gap-2 px-2'>
           <div className='h-10 w-10 border rounded-full'>
             <img src={settings} alt="" />
           </div>
