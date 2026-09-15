0ac5e8ebc7560aec3ac57508a43a305d7fc7c5dc
anas <anasrehman390@gmail.com>
2026-09-15 19:05:35 +0500

    feat(Pages/Auth/SignUp): form handling.
    - Create local state to store the from data.
    - Create the event handlers that stores the data to state.
    - Import the registerUser authSlice async thunk.
    - Dispatch the registerUser upon form submission
    - Use formdata as a param in the thunk.
    - Import and add the EyeIcon and EyeSlashIcon in the Passwords input fields.
    - Add the visible and invisible passwords functionality to passwords
      fields.

--- client/src/Pages/Auth/SignUp.jsx
+++ client/src/Pages/Auth/SignUp.jsx
@@ -1,44 +1,114 @@
+import { useState } from "react";
+import { useDispatch } from 'react-redux'
 import { NavLink } from "react-router";
+import { EyeIcon, EyeSlashIcon } from '../../Icons/Icons'
+import { registerUser } from "../../Features/authSlice";
 
 function SignUp() {
+  const dispatch = useDispatch();
+  // USER FORM DATA:
+  const [formdata, setFormdata] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormdata(prev => {
+      return {
+        ...prev, [name]: value
+      }
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(registerUser(formdata))
+    setFormdata({
+      username: '',
+      email: '',
+      password: ''
+    })
+  }
+
+  // TOGGLE PASSWORD VISIBLE OR INVISIBLE:
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  function togglePasswordVisible() {
+    setPasswordVisible(!passwordVisible);
+  }
+  function toggleConfirmPasswordVisible() {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  }
   return (
     <div className="h-auto w-full sm:w-120 p-8 flex-col-start justify-between gap-2 shadow-2xl  bg-slate-50">
       <h1 className="text-amber-600">SignUp</h1>
-      <form className="flex flex-col gap-4 w-full text-amber-900">
+      <form
+        onSubmit={handleSubmit} // SEND DATA TO AUTHSLICE
+        className="flex flex-col gap-4 w-full text-amber-900">
+
         <label className="flex flex-col gap-1.5 font-medium">
-          Username:
-          <input
-            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
-            type="text"
-            placeholder="Enter testing username" />
+          <span>Username:</span>
+          <div className="bg-[#F1F1F1] border-b w-full">
+            <input
+              className="w-full p-1.5 rounded-sm outline-none font-light"
+              name="username"
+              type="text"
+              value={formdata.username}
+              onChange={handleChange}
+              placeholder="Enter testing username" />
+          </div>
         </label>
+
         <label className="flex flex-col gap-1.5 font-medium">
-          Email:
-          <input
-            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
-            type="text"
-            placeholder="Enter testing email" />
+          <span>Email:</span>
+          <div className="bg-[#F1F1F1] border-b w-full">
+            <input
+              className="w-full p-1.5 rounded-sm outline-none font-light"
+              name="email"
+              type="text"
+              value={formdata.email}
+              onChange={handleChange}
+              placeholder="Enter testing email" />
+          </div>
         </label>
+
         <label className="flex flex-col gap-1.5 font-medium">
-          Password:
-          <input
-            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
-            type="password"
-            placeholder="Enter testing password" />
+          <span>Password:</span>
+          <div className="flex bg-[#F1F1F1] border-b w-full">
+            <input
+              className="w-full p-1.5 rounded-sm outline-none font-light"
+              name="password"
+              type={passwordVisible ? "text" : "password"}
+              value={formdata.password}
+              onChange={handleChange}
+              placeholder="Enter testing password" />
+            <div className="flex items-center mr-1 cursor-pointer p-1" onClick={togglePasswordVisible}>
+              {passwordVisible ? <EyeSlashIcon /> : <EyeIcon />}
+            </div>
+          </div>
         </label>
+
         <label className="flex flex-col gap-1.5 font-medium">
-            Confirm Password:
-          <input
-            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
-            type="password"
-            placeholder="Enter testing password again" />
+          <span>Confirm Password:</span>
+          <div className="flex bg-[#F1F1F1] border-b w-full">
+            <input
+              className="w-full p-1.5 rounded-sm outline-none font-light"
+              type={confirmPasswordVisible ? "text" : "password"}
+              placeholder="Enter testing password again" />
+            <div className="flex items-center mr-1 cursor-pointer p-1" onClick={toggleConfirmPasswordVisible}>
+              {confirmPasswordVisible ? <EyeSlashIcon /> : <EyeIcon />}
+            </div>
+          </div>
         </label>
+
         <button className="bg-amber-900 text-white p-1.5 rounded-sm cursor-pointer mt-2">SUBMIT</button>
       </form>
       <div className="flex gap-0.5">
         <p>Already registered?</p>
         <NavLink
-        to={'/login'}
+          to={'/login'}
         >
           Sign-in
         </NavLink>
