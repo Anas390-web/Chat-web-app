840e7f1ae0e10c6e1c554d3bd500f27549ea5752
anas <anasrehman390@gmail.com>
2026-09-15 17:57:51 +0500

    feat(app.js): add middlwares,routes & API listener
    - Add the cors to connect with the React server
    - Add json parser middleware.
    - Add middleware for static files
    - Add the Auth route for all the Auth requests
    - Add the async function that connects to DB and then listens to API
      requests.

--- server/app.js
+++ server/app.js
@@ -1,4 +1,31 @@
+import 'dotenv/config'
 import express from 'express'
+import connectToDB from './connectDB/connectDB.js'
+import cors from 'cors'
+import authRouter from './routes/Users/users.js'
 
 const app = express();
+const port = process.env.PORT || 3000;
 
+// MIDDLEWARES:
+app.use(cors({ origin: process.env.VITE_REACT_BASE_SERVER }))
+app.use(express.json())
+app.use(express.static('./public'))
+
+// ROUTES
+app.use('/api/v1/auth', authRouter)
+
+// DB CONNECTION AND THEN LISTENS TO THE API REQUESTS:
+const start = async () => {
+   try {
+      await connectToDB(process.env.MONGO_URI);
+      console.log('CONNECTED TO DB');
+      app.listen(port, () => {
+         console.log(`Server is listening on ${port}...`)
+      })
+   } catch (error) {
+      console.log(error.message)
+   }
+}
+
+start();
