b2ef230c5f44170be9a4d9251566994e9bce4afa
anas <anasrehman390@gmail.com>
2026-09-16 17:52:08 +0500

    feat(Pages/auth): login form handling & navigation
    - Import loginUser from the authSlice.
    - Put input fields inside the divs and style them.
    - Create event handlers for form handling.
    - Create local state formData to save the User login details.
    - Dispatch loginUser action upon form submission.
    - Add the formData as a param in the loginUser action.
    - Access the token from the auth state from store.
    - Put a check inside the useEffect if the token is present, Navigate to
      the dashboard.

--- client/src/Pages/Auth/Login.jsx
+++ client/src/Pages/Auth/Login.jsx
@@ -1,30 +1,80 @@
-import { NavLink } from "react-router"
+import { NavLink } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../Features/authSlice.js"
+import { useState, useEffect } from "react";
+
 
 function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // IF TOKEN IS PRESENT, NAVIGATE TO DASHBOARD:
+  const store = useSelector((store) => store.auth)
+  const { token } = store;
+  useEffect(() => {
+    if (token) {
+      navigate('/dashboard')
+    }
+  }, [token, navigate])
+
+  // USER FORM HANDLING:
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  // EVENT HANDLERS:
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prev) => {
+      return ({
+        ...prev, [name]: value
+      })
+    })
+  }
+
+  // DISPATCH THE LOGIN USER ASYNC THUNK API REQUEST:
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(loginUser(formData));
+  }
+
   return (
     <div className="h-auto w-full sm:w-120 p-8 flex-col-start justify-between gap-2 shadow-2xl  bg-slate-50">
       <h1 className="text-amber-600">Login</h1>
-      <form className="flex flex-col gap-4 w-full">
+      <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit}>
         <label className="flex flex-col gap-1.5 font-medium">
           Email:
-          <input
-            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
-            type="text"
-            placeholder="Enter testing email" />
+          <div className="bg-[#F1F1F1] border-b w-full">
+            <input
+              className="w-full p-1.5 rounded-sm outline-none font-light"
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter testing email" />
+          </div>
         </label>
         <label className="flex flex-col gap-1.5 font-medium">
           Password:
-          <input
-            className="bg-[#F1F1F1] border-b w-full p-1.5 rounded-sm outline-black font-light"
-            type="password"
-            placeholder="Enter testing password" />
+          <div className="bg-[#F1F1F1] border-b w-full">
+            <input
+              className="w-full p-1.5 rounded-sm outline-none font-light"
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter testing password" />
+          </div>
         </label>
         <button className="text-white bg-amber-900 p-1.5 rounded-sm cursor-pointer mt-2">LOGIN</button>
       </form>
       <div className="flex gap-0.5">
         <p>No account?</p>
         <NavLink
-        to={'/signup'}
+          to={'/signup'}
         >
           Create an account
         </NavLink>
