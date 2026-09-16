aefd63daff4c15d615855c1f8970a7135e7eb102
anas <anasrehman390@gmail.com>
2026-09-16 18:49:11 +0500

    feat(controllers/users): create auth callbacks
    - Import StatusCodes from the http-status-codes.
    - Import User from models.
    - Create the registerUser callback.
    - Save the User data to the DB, create token and respond with username
      and token in the callback.
    - Create loginUser callback.
    - Verify email and password, create a token.
    - Respond with username email and token for accessing protected routes.
    - Export registerUser and loginUser callbacks.

--- server/controllers/Users/users.js
+++ server/controllers/Users/users.js
@@ -1,12 +1,82 @@
+import { BadRequestError, UnauthenticatedError } from '../../errors/customApiErrors.js';
+import { StatusCodes } from 'http-status-codes'
+import User from '../../models/Users/User.js'
+
 const registerUser = async (req, res) => {
-   const { username, email, password } = req.body;
-   console.log(username, email, password)
-   res.json({
-      username,
-      email,
-      password
-   })
+   try {
+      const { username, email, password } = req.body;
+      // IF ANY USER DATA IS NOT PRESENT, THROW AN ERROR:
+      if (!username || !email || !password) {
+         return res.status(StatusCodes.BAD_REQUEST).json({
+            msg: 'Please provide username, email and password'
+         })
+      }
+      // SAVE USER DATA TO DB:
+      const user = await User.create({
+         username,
+         email,
+         password
+      })
+      // CREATE THE JWT TOKEN:
+      const token = user.createJwt();
+      if (!token) {
+         console.log('Token is not created')
+      }
+      // RESPONDING TO REQUEST:
+      res.status(StatusCodes.CREATED).json({
+         msg: 'User is created',
+         username: user.username,
+         token
+      })
+
+   } catch (error) {
+      console.log(error.message);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+         msg: 'Internal server error, Please try again later'
+      })
+   }
 }
 
+const loginUser = async (req, res) => {
+   try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+         return res.status(StatusCodes.BAD_REQUEST).json({
+            msg: 'Please provide email and password'
+         })
+      }
+      // FIND THE USER FROM DB
+      const user = await User.findOne({ email })
+      if (!user) {
+         console.log('User does not exist');
+         return res.status(StatusCodes.BAD_REQUEST).json({
+            msg: 'Invalid credentials'
+         })
+      }
+      // VERIFY PASSWORD:
+      const isPasswordCorrect = await user.comparePassword(password);
+      if (!isPasswordCorrect) {
+         return res.status(StatusCodes.UNAUTHORIZED).json({
+            msg: 'Invalid credentials'
+         })
+      }
+      // CREATION OF TOKEN:
+      const token = user.createJwt();
 
-export { registerUser }
+      // RESPOND THE API REQUEST WITH THE TOKEN:
+      res.status(StatusCodes.OK).json({
+         user: {
+            username: user.username,
+            email: user.email
+         },
+         token
+      })
+   } catch (error) {
+      console.log(error);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+         msg: 'Internal server error'
+      })
+   }
+}
+
+export { registerUser, loginUser }
