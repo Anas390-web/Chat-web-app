88a899525e999cecb1d962daeea01cbb6665c249
anas <anasrehman390@gmail.com>
2026-09-16 17:42:37 +0500

    feat(features/auth): additions in registerUser req
    - Add comments in registerUser async thunk.
    - Take token out of the data from the server.
    - Set token in the localStorage.
    - Make a signOut reducer.
    - Add the extra reducers using the payload gotten from the registration of users in the DB.
    - Export the signOut action.

--- client/src/Features/authSlice.js
+++ client/src/Features/authSlice.js
@@ -2,11 +2,12 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
    username: '',
-   token: '',
+   token: localStorage.getItem('accessToken') || '',
    isLoading: '',
    error: ''
 }
 
+// TO REGISTER USER IN THE DB:
 export const registerUser = createAsyncThunk(
    'auth/registerUser',
    async (userFormData, thunkApi) => {
@@ -21,11 +22,17 @@ export const registerUser = createAsyncThunk(
                userFormData
             )
          })
+         // PARSING JSON:
          const data = await response.json();
-         console.log('data' ,data);
-         if(!response.ok) {
+
+         // IF THERE IS NO RESPONSE, EXPLICITLY STOPS THE EXECUTION AS CATCH BLOCK DOES NOT CATCH THE HTTP STATUS CODES
+         if (!response.ok) {
             return thunkApi.rejectWithValue(data);
          }
+
+         // SETTING THE ACCESS TOKEN TO THE LOCALSTORAGE
+         const token = data.token;
+         localStorage.setItem('accessToken', token)
          return data;
       } catch (error) {
          console.log(error.message)
@@ -34,17 +41,37 @@ export const registerUser = createAsyncThunk(
    }
 )
 
+
+
 const authSlice = createSlice({
    name: 'register',
    initialState,
    reducers: {
-
+      signOut: (state) => {
+         localStorage.removeItem("accessToken");
+         state.token = ''
+      }
    },
    extraReducers: (builder) => {
-
+      builder
+         // FOR REGISTERING THE USER:
+         .addCase(registerUser.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(registerUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.username = action.payload.username
+         })
+         .addCase(registerUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+         })
+         
    }
 })
 
 const authReducer = authSlice.reducer;
+
+export const { signOut } = authSlice.actions;
 
 export { authReducer }
