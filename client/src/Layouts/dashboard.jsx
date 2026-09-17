cd9b46ef282e2a9e34e253a4674f9babf0a2866e
anas <anasrehman390@gmail.com>
2026-09-17 18:50:36 +0500

    feat(Layout/dashboard): dark/light mode
    - Create a local state of mode for dark and light modes.
    - Create event handlers that sets dark and light modes in the mode
      state.
    - Use useEffect that sets the theme to mode when mode changes in
      localStorage.
    - Mode takes its initial value from localStorage theme variable.
    - Sends the event handlers as props from Sidebar component.
    - Also send mode state through context attribute of Outlet.

--- client/src/Layouts/dashboard.jsx
+++ client/src/Layouts/dashboard.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Pages/Dashboard/Static-Sidebar/Sidebar.jsx'
+import { getAllUsers } from '../Features/authSlice.js';
+import { useDispatch, useSelector } from 'react-redux'
 
 function DashboardLayout() {
+   const dispatch = useDispatch();
+   // DARK/LIGHT MODE:
+   const [mode, setMode] = useState(
+      () => {
+         return localStorage.getItem("theme")
+      }
+   );
+
+   // THE MOMENT MODE IS CHANGED, CHANGE THE THEME TO MODE:
+   useEffect(() => {
+      localStorage.setItem("theme", mode)
+   }, [mode])
+
+   function handleDarkMode() {
+      setMode('dark');
+   }
+   function handleLightMode() {
+      setMode('light');
+   }
+
    return (
-      <div className='min-h-dvh overflow-hidden flex'>
-         <Sidebar />
+      <div
+         className={`min-h-dvh overflow-hidden flex ${mode === 'dark' && 'bg-[#111b21] text-gray-200'} ${mode === 'light' && 'bg-[#f1ebe7]'}`}>
+
+         <Sidebar
+            mode={mode}
+            handleDarkMode={handleDarkMode}
+            handleLightMode={handleLightMode} />
          <div className='flex-1 min-w-0 h-full'>
-            <Outlet />
+            <Outlet context={{ mode }} />
          </div>
       </div>
    )
