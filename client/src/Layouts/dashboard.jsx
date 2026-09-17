58737d32b05c22af9c49826a044ad771056770e2
anas <anasrehman390@gmail.com>
2026-09-17 19:12:08 +0500

    feat(Layout/dashboard): get all registered users
    - Get all the registered users upon dashboard mounting when user logs in
      or refreshes the screen.
    - Import getAllUsers async thunk function.
    - Dispatch it in the useEffect and sends the token as arguement to the
      function to be used as the Authorization purposes.
    - Create a check to mount the dashboard only if the token is present in
      the auth state.

--- client/src/Layouts/dashboard.jsx
+++ client/src/Layouts/dashboard.jsx
@@ -6,6 +6,8 @@ import { useDispatch, useSelector } from 'react-redux'
 
 function DashboardLayout() {
    const dispatch = useDispatch();
+   // ACCESSING TOKEN FROM THE STORE:
+   const { token } = useSelector((store) => store.auth);
    // DARK/LIGHT MODE:
    const [mode, setMode] = useState(
       () => {
@@ -25,18 +27,29 @@ function DashboardLayout() {
       setMode('light');
    }
 
+   // GET THE ARRAY OF ALL THE USERS:
+   useEffect(() => {
+      dispatch(getAllUsers(token))
+   }, [dispatch])
    return (
-      <div
-         className={`min-h-dvh overflow-hidden flex ${mode === 'dark' && 'bg-[#111b21] text-gray-200'} ${mode === 'light' && 'bg-[#f1ebe7]'}`}>
+      <>
+         {
+            token ?
+               <div
+                  className={`min-h-dvh overflow-hidden flex ${mode === 'dark' && 'bg-[#111b21] text-gray-200'} ${mode === 'light' && 'bg-[#f1ebe7]'}`}>
 
-         <Sidebar
-            mode={mode}
-            handleDarkMode={handleDarkMode}
-            handleLightMode={handleLightMode} />
-         <div className='flex-1 min-w-0 h-full'>
-            <Outlet context={{ mode }} />
-         </div>
-      </div>
+                  <Sidebar
+                     mode={mode}
+                     handleDarkMode={handleDarkMode}
+                     handleLightMode={handleLightMode} />
+                  <div className='flex-1 min-w-0 h-full'>
+                     <Outlet context={{ mode }} />
+                  </div>
+               </div>
+               :
+               <div>PAGE NOT FOUND</div>
+         }
+      </>
    )
 }
 
