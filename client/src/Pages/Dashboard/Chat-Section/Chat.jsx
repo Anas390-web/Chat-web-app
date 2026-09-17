21622d5b7927ddaa6336627991425ec5a3bffe57
anas <anasrehman390@gmail.com>
2026-09-17 21:21:17 +0500

    feat(Dashboard/Chat-Section): change border color
    - Change the border color from white to gray-600.

--- client/src/Pages/Dashboard/Chat-Section/Chat.jsx
+++ client/src/Pages/Dashboard/Chat-Section/Chat.jsx
@@ -8,9 +8,9 @@ function Chat() {
             <div className="flex-1 overflow-y-auto">
                Chat area
             </div>
-            <div className="bg-gray-200 min-h-16 flex items-center px-3 py-2 w-full box-border border border-gray-400 gap-2">
+            <div className="min-h-16 flex items-center px-3 py-2 w-full box-border border border-gray-600 gap-2">
                <input
-                  className="bg-white h-10 flex-1 min-w-0 px-3 outline-none rounded border"
+                  className="h-10 flex-1 min-w-0 px-3 outline-none rounded border border-gray-600"
                   type="text"
                />
                <div className='h-10 flex justify-center items-center'>
--- client/src/Pages/Dashboard/Chat-Section/Header.jsx
+++ client/src/Pages/Dashboard/Chat-Section/Header.jsx
@@ -2,9 +2,9 @@ const settings = '/images/settings.png'
 
 function Header() {
   return (
-    <header className="h-20 bg-blue-200 flex items-center px-4 shrink-0 w-full">
+    <header className="bg-[#FF9B51] h-20 flex items-center px-4 shrink-0 w-full border border-gray-600">
       <div className='flex'>
-        <div className='h-10 w-10 border rounded-full'>
+        <div className='h-10 w-10 border border-gray-600 rounded-full'>
           <img src={settings} alt="" />
         </div>
         <div>
