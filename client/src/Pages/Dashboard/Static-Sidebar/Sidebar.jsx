02f67ea5f09ba82c8fc86f35664f1f1b490e20e8
anas <anasrehman390@gmail.com>
2026-09-17 21:16:14 +0500

    feat(Dashboard/Static-Sidebar): dark/light mode
    - Save images path in the variables for the dark mode.
    - De-structure props in the Sidebar component.
    - Use mode state prop for the image changes if the mode is dark or
      light.
    - Add dark/light mode event handlers on Light and dark icons to switch
      dark and light mode.

--- client/src/Pages/Dashboard/Static-Sidebar/Sidebar.jsx
+++ client/src/Pages/Dashboard/Static-Sidebar/Sidebar.jsx
@@ -1,41 +1,49 @@
+import { LightIcon, DarkIcon } from '../../../Icons/Icons'
+
+// IMAGES FROM ROOT PUBLIC FOLDER:
 const Dashboard = '/images/Dashboard.png';
 const Chats = '/images/chats.png';
 const Groups = '/images/groups.png';
 const Settings = '/images/settings.png';
 const Archives = '/images/Archives.png';
-import { LightIcon, DarkIcon } from '../../../Icons/Icons'
+
+const WhiteDashboard = '/images/White-dashboard.png'
+const WhiteChats = '/images/White-chats.png'
+const WhiteGroups = '/images/White-groups.png'
+const WhiteArchives = '/images/White-archives.png'
+const WhiteSettings = '/images/White-settings.png'
 
 
-function Sidebar() {
+function Sidebar({ mode, handleLightMode, handleDarkMode }) {
   return (
-    <aside className='h-screen w-24 shrink-0 bg-gray-200 flex flex-col justify-between items-center px-2 py-1 font-semibold'>
+    <aside className='bg-[#FF9B51]  h-screen w-24 shrink-0 flex flex-col justify-between items-center px-2 py-1 font-semibold'>
       <div className='flex flex-col gap-4'>
-        <div className='sidebar-icons'>
-          <img className='size-7' src={Dashboard} alt="Dashboard Icon" />
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'}>
+          <img className='size-7' src={mode === 'dark' ? WhiteDashboard : Dashboard} alt="Dashboard Icon" />
         </div>
         <hr />
-        <div className='sidebar-icons'>
-          <img className='size-7' src={Chats} alt="Chats Icon" />
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'}>
+          <img className='size-7' src={mode === 'dark' ? WhiteChats : Chats} alt="Chats Icon" />
           <p>Chats</p>
         </div>
-        <div className='sidebar-icons'>
-          <img className='size-7' src={Groups} alt="Groups Icon" />
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'}>
+          <img className='size-7' src={mode === 'dark' ? WhiteGroups : Groups} alt="Groups Icon" />
           <p>Groups</p>
         </div>
-        <div className='sidebar-icons'>
-          <img className='size-7' src={Archives} alt="Archives Icon" />
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'}>
+          <img className='size-7' src={mode === 'dark' ? WhiteArchives : Archives} alt="Archives Icon" />
           <p>Archives</p>
         </div>
-        <div className='sidebar-icons'>
-          <img className='size-7' src={Settings} alt="Settings Icon" />
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'}>
+          <img className='size-7' src={mode === 'dark' ? WhiteSettings : Settings} alt="Settings Icon" />
           <p>Settings</p>
         </div>
       </div>
       <div className='flex flex-col justify-center items-center gap-4'>
-        <div className='sidebar-icons'>
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'} onClick={() => handleLightMode()}>
           <p>{<LightIcon />}</p>
         </div>
-        <div className='sidebar-icons'>
+        <div className={mode === 'dark' ? 'dark-sidebar-icons' : 'sidebar-icons'} onClick={() => handleDarkMode()}>
           <p>{<DarkIcon />}</p>
         </div>
       </div>
