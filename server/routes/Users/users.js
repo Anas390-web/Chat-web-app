4398229df1ddfc959ab78cc662023243ddcc89ef
anas <anasrehman390@gmail.com>
2026-09-17 21:33:31 +0500

    feat(routes/users): add route for getAllUsers API
    - Import getAllUsers callback and userAuthentication callback.
    - Create a route for the API request "getAllUsers".
    - Add userAuthentication first and then getAllUsers callback.

--- server/routes/Users/users.js
+++ server/routes/Users/users.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { registerUser, loginUser } from '../../controllers/Users/users.js';
+import { registerUser, loginUser, getAllUsers } from '../../controllers/Users/users.js';
+import userAuthentication from '../../middlewares/Auth/auth.js';
 
 const authRouter = express.Router();
 
 authRouter.route('/register').post(registerUser);
 authRouter.route('/login').post(loginUser);
+authRouter.route('/').get(userAuthentication, getAllUsers);
 
 
 export default authRouter
