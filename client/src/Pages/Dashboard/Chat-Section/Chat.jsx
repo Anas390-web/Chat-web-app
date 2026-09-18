d84c1018781155458ca245f488e7c24065b35019
anas <anasrehman390@gmail.com>
2026-09-18 17:58:59 +0500

    feat(Dashboard/Chat-section): add bg-image
    - Destructure mode using useOutletContext().
    - Add the background image according to dark and light mode in that user
      chat section.

--- client/src/Pages/Dashboard/Chat-Section/Chat.jsx
+++ client/src/Pages/Dashboard/Chat-Section/Chat.jsx
@@ -1,12 +1,15 @@
 import { SendIcon } from '../../../Icons/Icons.jsx'
-
+import { useOutletContext } from 'react-router-dom';
 
 function Chat() {
+   const { mode } = useOutletContext();
    return (
       <main className="h-full">
          <div className="flex flex-col h-full">
-            <div className="flex-1 overflow-y-auto">
-               Chat area
+            <div className={`flex-1 overflow-y-auto ${mode === 'dark' ? "bg-[url(/images/Black-Doodle.jpg)] bg-contain bg-center" : "bg-[url(/images/White-Doodle.jpg)] bg-contain bg-center"}`}>
+               <div>
+                  Chat area
+               </div>
             </div>
             <div className="min-h-16 flex items-center px-3 py-2 w-full box-border border border-gray-600 gap-2">
                <input
