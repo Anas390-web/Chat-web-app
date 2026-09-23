1a55abefeef7643c65087e4208fb62b2cd1dfcee
anas <anasrehman390@gmail.com>
2026-09-23 17:43:30 +0500

    feat(Layout/Dashboard): add socket connection
    - Import socket instance from Socket folder.
    - Access token from the store.auth.
    - Inside useEffect with token dependency, check if token is present.
    - Then establish a connection using event listener "connect" with Server.
    - Add a cleanup function to remove the event listener upon unmounting.

--- client/src/Layouts/dashboard.jsx
+++ client/src/Layouts/dashboard.jsx
@@ -3,23 +3,53 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from '../Pages/Dashboard/Static-Sidebar/Sidebar.jsx'
 import { getAllUsers } from '../Features/authSlice.js';
 import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom';
+import socket from '../Socket/socket.js'
 
 function DashboardLayout() {
    const dispatch = useDispatch();
+
+   // ACCESSING TOKEN FROM THE STORE:
+   const { token } = useSelector((store) => store.auth)
+   // SOCKET CONNECTION WITH SERVER:
+   const handleConnection = () => {
+      console.log('CONNECTED WITH SERVER')
+   }
+   useEffect(() => {
+      if (token) {
+         // ATTACHING TOKEN TO SEND TO THE SERVER FOR AUTHENTICATION:
+         socket.auth = { token };
+         socket.on('connect', handleConnection)
+
+         // CHECK IF SOCKET IS CONNECTED OR NOT:
+         if (socket.connected) {
+            handleConnection();
+         } else {
+            socket.connect();
+         }
+      }
+      // CLEANUP FUNCTION:
+      return () => {
+         // CLEAN UP THE EVENT LISTENER
+         socket.off('connect', handleConnection);
+         // DISCONNECTION EXISTS ON THE USER SIGNOUT
+      }
+   }, [token])
+
+
    // SWAP SIDEBAR COMPONENTS (CHATS, GROUPS, ARCHIVES) BASED ON THE ID:
    const [componentId, setComponentId] = useState(null);
    function handleSwapComponent(getCompId) {
       setComponentId(getCompId);
    }
-   // ACCESSING TOKEN FROM THE STORE:
-   const { token } = useSelector((store) => store.auth);
+   // const { token } = useSelector((store) => store.auth);
    // DARK/LIGHT MODE:
    const [mode, setMode] = useState(
       () => {
-         return localStorage.getItem("theme")
+         return localStorage.getItem("theme") || 'light' // fallback value
       }
    );
-   
+
    // THE MOMENT MODE IS CHANGED, CHANGE THE THEME TO MODE:
    useEffect(() => {
       localStorage.setItem("theme", mode)
@@ -34,8 +64,10 @@ function DashboardLayout() {
 
    // GET THE ARRAY OF ALL THE USERS:
    useEffect(() => {
-      dispatch(getAllUsers(token))
-   }, [dispatch])
+      if (token) {
+         dispatch(getAllUsers(token))
+      }
+   }, [dispatch, token])
    return (
       <>
          {
@@ -54,7 +86,18 @@ function DashboardLayout() {
                   </div>
                </div>
                :
-               <div>PAGE NOT FOUND</div>
+               <div className='min-h-dvh flex justify-center items-center'>
+                  <div className='h-full sm:h-100 w-full sm:w-200 flex flex-col items-center justify-evenly'>
+                     <h1 className='font-mono text-red-700 text-6xl'>PAGE NOT FOUND</h1>
+                     <div className='flex gap-2 bg-amber-400'>
+                        <p>Go back to the Login page!</p>
+                        <NavLink
+                           to={'/login'}>
+                           Login
+                        </NavLink>
+                     </div>
+                  </div>
+               </div>
          }
       </>
    )
