59f401bfe3acfe3ad7eb13e5eb6c98effd11c719
anas <anasrehman390@gmail.com>
2026-09-23 17:31:09 +0500

    feat(src/socket): create socket instance
    - Create a folder named Socket in src.
    - Create a file named socket.js in it.
    - Import io from socket.io-client.
    - Create a socket instance.
    - Export it.

---
+++ client/src/Socket/socket.js
@@ -0,0 +1,10 @@
+import { io } from 'socket.io-client'
+
+const URL = 'http://localhost:3000'
+
+const socket = io(URL, {
+   autoConnect: false,
+   transports: ['websocket', 'polling']
+})
+
+export default socket;
