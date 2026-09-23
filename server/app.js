48094700e00fa89b1fa2d5d0a2c47379688e994c
anas <anasrehman390@gmail.com>
2026-09-23 19:03:13 +0500

    feat(root/app.js): socket connection
    - Import createServer from http.
    - Import Server class from socket.io.
    - Import jwt from jsonwebtoken.
    - Create an http server which wires app(express) in it.
    - Create an io instance which wires httpServer in it and set cors origin
      to React server and allows Get and post method.
    - Create a token authentication middleware coming from client to verify  token is valid or not.
    - Establish a connection with client server using io instance.
    - Add a socket listener that listens form client disconnection.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 import 'dotenv/config'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import express from 'express'
 import connectToDB from './connectDB/connectDB.js'
 import cors from 'cors'
+import jwt from 'jsonwebtoken';
 import authRouter from './routes/Users/users.js'
 import contactsRouter from './routes/Contacts/contacts.js'
 
 const app = express();
+const httpServer = createServer(app);
 const port = process.env.PORT || 3000;
 
 // MIDDLEWARES:
@@ -17,12 +21,47 @@ app.use(express.static('./public'))
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/contacts', contactsRouter);
 
+// SOCKET CONNECTION:
+const io = new Server(httpServer, {
+   cors: {
+      origin: process.env.VITE_REACT_BASE_SERVER,
+      methods: ['GET', 'POST']
+   }
+});
+
+// TOKEN AUTHENTICATION:
+io.use((socket, next) => {
+   try {
+      const token = socket.handshake.auth.token;
+      const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      socket.user = payload;
+      if (!payload) {
+         return next(new Error('Authentication error: Token missing'))
+      }
+      next();
+   } catch (error) {
+      return next(new Error('Authentication error: Invalid token'));
+   }
+})
+
+// CONNECTION WITH CLIENT:
+
+io.on('connection', (socket) => {
+   const { userId } = socket.user;
+   console.log('CONNECTED WITH CLIENT', socket.id)
+
+   // UPON USER DISCONNECTING:
+   socket.on('disconnect', () => {
+      console.log('CLIENT DISCONNECTED', socket.id);
+   })
+})
+
 // DB CONNECTION AND THEN LISTENS TO THE API REQUESTS:
 const start = async () => {
    try {
       await connectToDB(process.env.MONGO_URI);
       console.log('CONNECTED TO DB');
-      app.listen(port, () => {
+      httpServer.listen(port, () => {
          console.log(`Server is listening on ${port}...`)
       })
    } catch (error) {
