182ed2800e08c3c7304a21d28cebffc835852953
anas <anasrehman390@gmail.com>
2026-09-25 23:39:27 +0500

    feat(root/store.js): add messagesReducer
    - Store messagesReducer in the messages property inside the reducer in
      store.

--- client/store.js
+++ client/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./src/Features/authSlice.js";
 import { contactListReducer } from "./src/Features/contactsSlice.js";
+import messagesReducer from "./src/Features/messagesSlice.js";
 
 const store = configureStore({
    reducer: {
       auth: authReducer,
-      contacts: contactListReducer
+      contacts: contactListReducer,
+      messages: messagesReducer
    }
 })
 
